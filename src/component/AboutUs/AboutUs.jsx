dc3181553351809f315eb9817235edba10aa0b58
oken3903 <cuongluumd@gmail.com>
2023-07-31 21:59:10 +0700

    minhasdsad

--- src/component/AboutUs/AboutUs.jsx
+++ src/component/AboutUs/AboutUs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 import Footer from "../Footer/Footer";
 const AboutUs = () => {
-  return <div w-full ></div>;
+  return <div w-full >sadasfas</div>;
 };
 
 export default AboutUs;
